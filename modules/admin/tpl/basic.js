6e5bfaf59b55208df78838f7584b3b4a2c69e838
Dobronszki János <janosdobronszki@citromail.hu>
2012-07-25 12:59:09 +0200

    Added icon to show position on admin interface.

--- modules/admin/tpl/basic.js
+++ modules/admin/tpl/basic.js
@@ -4,6 +4,14 @@ $("a.delete").click(function() {
   return confirm("Are you sure want to delete this?");
 });
 
-$("a.delete").html("&#8212;")
+$("a.delete").html("&#8212;");
+
+var pathname = document.location.pathname;
+$("a").each(
+function(index, elem) {
+	if ($(elem).attr("href") == pathname) {
+		$(elem).html("<img class=\"you-are-here\" src=\"/tpl/admin/play_9x12.png\" />" + $(elem).html());
+	}
+})
 
 })
